b121b7a8beca8db37bb2733b27e91fdef82ce3be
ssssssmokin <ssssssmokin@users.noreply.github.com>
2019-07-25 15:43:34 +0300

    +

--- php/send_ua.php
+++ php/send_ua.php
@@ -2,7 +2,7 @@
 $result = mail ("interclasskhm@gmail.com", "Letter from site", " Name: $_POST[yourname] \n Email: $_POST[email] \n Tel: $_POST[tel] \n Message: $_POST[message]. \r\n");
  if ($result) { 
     echo "<script>alert(\"Ваше повідомлення надіслано!\");</script>"; 
-    echo '<script>window.location.href = "http://interclass.test/"</script>;';
+    echo '<script>window.location.href = "http://interclass.net.ua/"</script>;';
     }
 
     else { 
